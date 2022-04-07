2274697246efcbb5366bcacaadb1c582963d8a1c
gabrielrauch <gabrielrauch@protonmail.com>
2022-04-06 21:46:21 -0300

    Formatting values on table

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -3,19 +3,21 @@ import { api } from '../../services/api'
 import { Container } from './styles'
 
 interface Transaction {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
+  id: number
+  title: string
+  amount: number
+  type: string
+  category: string
+  createdAt: string
 }
 
 export function TransactionsTable() {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   useEffect(() => {
-    api.get('transactions').then((response) => setTransactions(response.data.transactions))
+    api
+      .get('transactions')
+      .then((response) => setTransactions(response.data.transactions))
   }, [])
 
   return (
@@ -34,9 +36,14 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>{new Intl.DateTimeFormat('en-US').format(new Date(transaction.createdAt))}</td>
             </tr>
           ))}
         </tbody>
